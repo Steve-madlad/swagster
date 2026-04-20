33ee09b7023b2b3e89332f73ac5b91855de11fcd
Estifanos Wendimu <teffsauce2@gmail.com>
2026-04-20 19:55:59 +0300

    removed strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,21 @@
-import { StrictMode } from 'react';
+import { Analytics } from '@vercel/analytics/react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
-import './index.css';
-import App from './App.tsx';
 import { Toaster } from 'sonner';
-import { Analytics } from '@vercel/analytics/react';
+import App from './App.tsx';
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <Router>
-      <App />
-      <Analytics />
-      <Toaster
-        toastOptions={{
-          style: {
-            background: 'var(--primary)',
-            color: 'var(--primary-foreground)',
-          },
-        }}
-      />
-    </Router>
-  </StrictMode>,
+  <Router>
+    <App />
+    <Analytics />
+    <Toaster
+      toastOptions={{
+        style: {
+          background: 'var(--primary)',
+          color: 'var(--primary-foreground)',
+        },
+      }}
+    />
+  </Router>,
 );
