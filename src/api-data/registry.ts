c64de7e28bc697246fb26295024a6421bdc2e6c8
Estifanos Wendimu <teffsauce2@gmail.com>
2026-07-06 14:24:54 +0300

    Minor auth description registry update

--- src/api-data/registry.ts
+++ src/api-data/registry.ts
@@ -1778,7 +1778,7 @@ const registry = {
         headerName: 'Authorization',
         strategy: 'manual-token',
         instruction:
-          'Obtain your JWT from your Bank Co account and paste it directly into the authorization field.',
+          'Obtain your JWT from your firebase and paste it directly into the authorization field.',
       },
     },
   ],
